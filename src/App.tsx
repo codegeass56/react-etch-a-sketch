a8da782dfd7d97d8010d922baf6b7dfd275f40da
Siddharth Panjwani <56246862+codegeass56@users.noreply.github.com>
2024-02-20 21:29:08 +0400

    Add logic to color the grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./components/Button";
 import FlexContainer from "./components/FlexContainer";
 import Grid from "./components/Grid";
@@ -7,7 +7,14 @@ import Slider from "./components/Slider";
 import GridItem from "./components/GridItem";
 
 function App() {
-  const [shadingValue, setShadingValue] = useState(0);
+  const [gridIsClicked, setGridIsClicked] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("mouseup", () => {
+      setGridIsClicked(false);
+    });
+  }, []);
+
   return (
     <div className="app">
       <Header />
@@ -17,7 +24,12 @@ function App() {
       </FlexContainer>
       <Grid>
         {Array.from({ length: Math.pow(16, 2) }, (_, i) => (
-          <GridItem key={i} />
+          <GridItem
+            key={i}
+            onGridEnable={() => setGridIsClicked(true)}
+            onGridDisable={() => setGridIsClicked(false)}
+            gridIsClicked={gridIsClicked}
+          />
         ))}
       </Grid>
       <FlexContainer>
--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,10 +1,46 @@
-function GridItem() {
-  const alphaVal = 0;
+import { useState } from "react";
+
+type Props = {
+  onGridEnable: () => void;
+  onGridDisable: () => void;
+  gridIsClicked: boolean;
+};
+
+function GridItem({ onGridEnable, onGridDisable, gridIsClicked }: Props) {
+  const [alpha, setAlpha] = useState(0);
   const styles: React.CSSProperties = {
-    backgroundColor: `rgba(0,0,0,${alphaVal})`,
+    backgroundColor: `rgba(0,0,0,${alpha})`,
     border: "1px solid rgb(204, 204, 204)",
   };
-  return <div style={styles} className="gridItem"></div>;
+
+  function handleShade() {
+    if (!gridIsClicked) return;
+    colorCell();
+  }
+
+  function colorCell() {
+    if (alpha > 0.9) {
+      setAlpha(1);
+    } else {
+      setAlpha((alpha) => Number((alpha + 0.1).toFixed(1)));
+    }
+  }
+
+  return (
+    <div
+      onDragStart={(e) => e.preventDefault()}
+      style={styles}
+      className="gridItem"
+      onMouseDown={() => {
+        colorCell();
+        onGridEnable();
+      }}
+      onMouseUp={onGridDisable}
+      onMouseEnter={() => {
+        handleShade();
+      }}
+    ></div>
+  );
 }
 
 export default GridItem;
